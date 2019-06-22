2cea916762358e65cf26aa48007a684e85220d6e
Christian Fischer <cf@fisch3r.de>
2019-06-22 13:39:08 +0200

    initial setup

---
+++ src/test/kotlin/jgiven/asciidoc/gradle/plugin/JgivenAsciidocPluginTest.kt
@@ -0,0 +1,11 @@
+package jgiven.asciidoc.gradle.plugin
+
+import org.junit.jupiter.api.Test
+
+class JgivenAsciidocPluginTest {
+
+    @Test
+    internal fun shouldBeLoaded() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
