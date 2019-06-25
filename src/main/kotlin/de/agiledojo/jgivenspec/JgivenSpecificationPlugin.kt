3762c60ff03ba52d0733c4f36293999ad51d8f68
Christian Fischer <cf@fisch3r.de>
2019-06-25 19:17:21 +0200

    e2e test

--- src/main/kotlin/de/agiledojo/jgivenspec/JgivenSpecificationPlugin.kt
+++ src/main/kotlin/de/agiledojo/jgivenspec/JgivenSpecificationPlugin.kt
@@ -3,16 +3,22 @@ package de.agiledojo.jgivenspec
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.io.File
+import java.io.FileWriter
 
 class JgivenSpecificationPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.task("jgivenSpecReport") {
-            val jgivenResultFile = File(project.projectDir, "jgiven-reports/result.json")
-            if (jgivenResultFile.exists()) {
-                val text = jgivenResultFile.readText()
-                File(project.projectDir,"specification").mkdir()
-                File(project.projectDir,"specification/spec.pdf").writeText(text)
+
+            val reportsDir = File(project.projectDir, "jgiven-reports")
+            if (reportsDir.exists()) {
+                val importer = JGivenImporter(AsciiDocExporter(object : Writer {
+                    override fun write(content: String) {
+                        val specFile = File(project.projectDir, "spec.html")
+                        FileWriter(specFile).write(content)
+                    }
+                }))
+                importer.read(reportsDir)
             }
         }
     }
