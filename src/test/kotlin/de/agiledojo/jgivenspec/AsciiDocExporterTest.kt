833b52976ddcdc8ebabbf7af289025451510e527
Christian Fischer <cf@fisch3r.de>
2019-06-25 06:16:39 +0200

    export spec as html

--- src/test/kotlin/de/agiledojo/jgivenspec/AsciiDocExporterTest.kt
+++ src/test/kotlin/de/agiledojo/jgivenspec/AsciiDocExporterTest.kt
@@ -3,7 +3,9 @@ package de.agiledojo.jgivenspec
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
 import org.mockito.Mock
+import org.mockito.Mockito.contains
 import org.mockito.Mockito.verify
 import org.mockito.junit.jupiter.MockitoExtension
 
@@ -15,16 +17,11 @@ class AsciiDocExporterTest {
     private lateinit var writer: Writer;
 
     @Test
-    internal fun shouldExportAsHTML() {
+    internal fun shouldExportSpecificationAsHTML() {
         val exporter = AsciiDocExporter(writer)
         exporter.addTemplate("== {{name}}")
         val specification = Specification("Spec 1")
         exporter.toHtml(specification)
-        verify(writer).write("<div class=\"sect1\">\n" +
-                "<h2 id=\"_spec_1\">Spec 1</h2>\n" +
-                "<div class=\"sectionbody\">\n" +
-                "\n" +
-                "</div>\n" +
-                "</div>")
+        verify(writer).write(ArgumentMatchers.contains("<h2 id=\"_spec_1\">Spec 1</h2>"))
     }
 }
